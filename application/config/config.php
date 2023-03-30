ddaa6fed642d965217ae7d0affb87316c2a1489f
DjihadAkbar <mochdjihad@gmail.com>
2023-03-30 14:38:20 +0800

    Auto Count

--- application/config/config.php
+++ application/config/config.php
@@ -23,8 +23,8 @@ defined('BASEPATH') or exit('No direct script access allowed');
 | a PHP script and you can easily do that on your own.
 |
 */
-$config['base_url'] = 'http://localhost/luwuk/';
-// $config['base_url'] = 'https://recons-luwuk.com/';
+// $config['base_url'] = 'http://localhost/luwuk/';
+$config['base_url'] = 'https://recons-luwuk.com/';
 
 
 /*
@@ -187,9 +187,9 @@ $config['permitted_uri_chars'] = 'a-z 0-9~%.:_\-';
 |
 */
 $config['enable_query_strings'] = FALSE;
-$config['controller_trigger'] = 'c';
-$config['function_trigger'] = 'm';
-$config['directory_trigger'] = 'd';
+$config['controller_trigger']   = 'c';
+$config['function_trigger']     = 'm';
+$config['directory_trigger']    = 'd';
 
 /*
 |--------------------------------------------------------------------------
@@ -385,13 +385,13 @@ $config['encryption_key'] = '';
 | except for 'cookie_prefix' and 'cookie_httponly', which are ignored here.
 |
 */
-$config['sess_driver'] = 'files';
-$config['sess_cookie_name'] = 'ci_session';
-$config['sess_samesite'] = 'Lax';
-$config['sess_expiration'] = 7200;
-$config['sess_save_path'] = NULL;
-$config['sess_match_ip'] = FALSE;
-$config['sess_time_to_update'] = 300;
+$config['sess_driver']             = 'files';
+$config['sess_cookie_name']        = 'ci_session';
+$config['sess_samesite']           = 'Lax';
+$config['sess_expiration']         = 7200;
+$config['sess_save_path']          = NULL;
+$config['sess_match_ip']           = FALSE;
+$config['sess_time_to_update']     = 300;
 $config['sess_regenerate_destroy'] = FALSE;
 
 /*
@@ -410,10 +410,10 @@ $config['sess_regenerate_destroy'] = FALSE;
 |       'cookie_httponly') will also affect sessions.
 |
 */
-$config['cookie_prefix'] = '';
-$config['cookie_domain'] = '';
-$config['cookie_path'] = '/';
-$config['cookie_secure'] = FALSE;
+$config['cookie_prefix']   = '';
+$config['cookie_domain']   = '';
+$config['cookie_path']     = '/';
+$config['cookie_secure']   = FALSE;
 $config['cookie_httponly'] = FALSE;
 $config['cookie_samesite'] = 'Lax';
 
@@ -459,11 +459,11 @@ $config['global_xss_filtering'] = FALSE;
 | 'csrf_regenerate' = Regenerate token on every submission
 | 'csrf_exclude_uris' = Array of URIs which ignore CSRF checks
 */
-$config['csrf_protection'] = FALSE;
-$config['csrf_token_name'] = 'csrf_test_name';
-$config['csrf_cookie_name'] = 'csrf_cookie_name';
-$config['csrf_expire'] = 7200;
-$config['csrf_regenerate'] = TRUE;
+$config['csrf_protection']   = FALSE;
+$config['csrf_token_name']   = 'csrf_test_name';
+$config['csrf_cookie_name']  = 'csrf_cookie_name';
+$config['csrf_expire']       = 7200;
+$config['csrf_regenerate']   = TRUE;
 $config['csrf_exclude_uris'] = array();
 
 /*
@@ -531,6 +531,6 @@ $config['rewrite_short_tags'] = FALSE;
 | Comma-separated:	'10.0.1.200,192.168.5.0/24'
 | Array:		array('10.0.1.200', '192.168.5.0/24')
 */
-$config['proxy_ips'] = '';
+$config['proxy_ips']           = '';
 $config['javascript_location'] = 'http://localhost/codeigniter/themes/js/jquery/';
 $config['javascript_ajax_img'] = 'images/ajax-loader.gif';
