e0acf8514578c033f5166404da9387feed355694
DjihadAkbar <mochdjihad@gmail.com>
2023-03-31 10:18:52 +0800

    Menampilkan sesuai bulan

--- application/models/Income_model.php
+++ application/models/Income_model.php
@@ -622,7 +622,7 @@ class Income_model extends CI_Model
                 if ($this->session->userdata('logged_in') && $this->session->userdata['jabatan'] == 'NAHKODA') {
             $this->db->where('ferry.ferry', $pelabuhan);
         }
-        $this->db->where('day(date)', date("d"));
+        $this->db->where('day(date)', date("d-1"));
         $this->db->where('MONTHname(date)', date("F"));
         $this->db->where('year(date)', date("Y"));
         // $this->db->where('entry_data.id_trip', 'REGULER');
