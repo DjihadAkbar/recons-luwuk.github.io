12cd127b6f47142fe18115e4718a02442dce31d9
DjihadAsus <mochdjihad@gmail.com>
2023-05-04 20:21:12 -0700

    Separator

--- application/controllers/Report.php
+++ application/controllers/Report.php
@@ -1652,7 +1652,7 @@ class Report extends CI_Controller
             );
 
             $sheet = $sheet->getStyle('A1:J45')->applyFromArray($styleArray);
-            $sheet->getStyle('A1:A3')->getNumberFormat()->setFormatCode('#,##0.00');
+            $spreadsheet->getActiveSheet()->getStyle('A1:K1')->getNumberFormat()->setFormatCode('[Blue][>=3000]$#,##0;[Red][<0]$#,##0;$#,##0');
 
             $no++;
         }
