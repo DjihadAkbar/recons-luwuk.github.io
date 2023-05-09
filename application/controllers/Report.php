ef9078652251a54b16f5b2f0c624c5666ec5f1be
DjihadAsus <mochdjihad@gmail.com>
2023-05-09 14:10:05 +0800

    Baru ka sadar

--- application/controllers/Report.php
+++ application/controllers/Report.php
@@ -1237,7 +1237,7 @@ class Report extends CI_Controller
         $pelabuhanReport = $this->input->post('pelabuhan_asal_report');
         $jamReport = $this->input->post('jam');
         $bulan = [1 => "JANUARI", "FEBURARI", "MARET", "APRIL", "MEI", "JUNI", "JULI", "AGUSTUS", "SEPTEMBER", "OKTOBER", "NOVEMBER", "DESEMBER"];
-        $listLintasan = ["GABUNGAN", "GORONTALO", "SULTENG"];
+        $listLintasan = ["GABUNGAN", "GABUNGAN GORONTALO", "SULTENG"];
         foreach ($this->Master_model->lintasanSpv() as $row) {
             array_push($listLintasan, $row['lintasan']);
         }
@@ -1262,7 +1262,7 @@ class Report extends CI_Controller
                 $batas = "";
             } elseif (str_contains($lintasan, "SULTENG")) {
                 $batas = "AND NOT (route = 'GORONTALO-WAKAI' OR route = 'GORONTALO-PAGIMANA' OR route = 'MARISA-DOLONG')";
-            } elseif (str_contains($lintasan, "GORONTALO")) {
+            } elseif (str_contains($lintasan, "GABUNGAN GORONTALO")) {
                 $batas = "AND (route = 'GORONTALO-WAKAI' OR route = 'GORONTALO-PAGIMANA' OR route = 'MARISA-DOLONG')";
             } else {
                 $batas = "AND ROUTE = '{$lintasan}'";
