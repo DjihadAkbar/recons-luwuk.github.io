6f54d52eb279951e4c9e6a8908d80303ef252dcb
DjihadAkbar <mochdjihad@gmail.com>
2023-05-22 14:05:18 +0800

    Perubahan Hari ini

--- application/controllers/Archive.php
+++ application/controllers/Archive.php
@@ -1,6 +1,6 @@
 <?php
 
-class Admin extends CI_Controller
+class Archive extends CI_Controller
 {
     public function __construct()
     {
@@ -13,32 +13,9 @@ class Admin extends CI_Controller
         $data['title'] = 'Archive';
         $data['contentView'] = 'archive/index';
         $data['pendapatan'] = $this->Income_model->pendapatan();
-        $data['incomePerRoute'] = $this->Income_model->incomePerRoute();
-        $data['incomePerHarbour'] = $this->Income_model->incomePerHarbour();
-        $data['incomePerShip'] = $this->Income_model->incomePerShip();
-        $data['incomeDaily'] = $this->Income_model->incomeDaily();
-        $data['incomeDailyPerShip'] = $this->Income_model->incomeDailyPerShip();
-        $data['incomeDailyPerHarbour'] = $this->Income_model->incomeDailyPerHarbour();
-        $data['totalDaily'] = $this->Income_model->totalDaily();
-        $data['totalDailyPerShip'] = $this->Income_model->totalDailyPerShip();
-        $data['totalDailyPerHarbour'] = $this->Income_model->totalDailyPerHarbour();
 
 
         $this->load->view('template/dashboard/body', $data);
         // $this->load->view('pages/dashboard', $data);
     }
-
-    public function accountSettings(){
-        if($this->session->userdata['type'] != 'ADMIN'){
-            redirect(base_url('dashboard'));
-        } else {
-            $data['title'] = 'Account Settings';
-            $data['contentView'] = 'admin/accountSettings';
-
-            $data['user_accounts'] = $this->Admin_model->user_accounts();
-
-            $this->load->view('template/dashboard/body', $data);
-        }
-    }
-
 }
