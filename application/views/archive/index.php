74df1f292179d7f81970b3e5e422b2353bb61ea2
DjihadAkbar <mochdjihad@gmail.com>
2023-05-22 16:48:36 +0800

    Perubahan Hari ini

--- application/views/archive/index.php
+++ application/views/archive/index.php
@@ -1,9 +1,8 @@
 <div class="card-header d-flex justify-content-between align-items-center text-center"> 
     <div class="tambah-data">  
         <?php
-        $dataAnchor = ['class' => 'btn btn-dark text-light akses-button','data-toggle'=>'modal','data-target'=>'#exampleModal'];
-        echo anchor('', '+ Tambah Data', $dataAnchor);
-        // echo anchor('archive/addArchive', '+ Tambah Data', $dataAnchor);
+        $dataAnchor = ['class' => 'btn btn-dark text-light akses-button'];
+        echo anchor('archive/addArchive', '+ Tambah Data', $dataAnchor);
         ?>
         
     </div> 
@@ -87,21 +86,23 @@
         </div>
     </div>
 </div>
+<!-- Button trigger modal -->
 <button type="button" class="btn btn-primary" data-toggle="modal" data-target="#exampleModal">
   Launch demo modal
 </button>
 
-<div class="modal" tabindex="-1" role="dialog">
+<!-- Modal -->
+<div class="modal fade" id="exampleModal" tabindex="-1" role="dialog" aria-labelledby="exampleModalLabel" aria-hidden="true">
   <div class="modal-dialog" role="document">
     <div class="modal-content">
       <div class="modal-header">
-        <h5 class="modal-title">Modal title</h5>
+        <h5 class="modal-title" id="exampleModalLabel">Modal title</h5>
         <button type="button" class="close" data-dismiss="modal" aria-label="Close">
           <span aria-hidden="true">&times;</span>
         </button>
       </div>
       <div class="modal-body">
-        <p>Modal body text goes here.</p>
+        ...
       </div>
       <div class="modal-footer">
         <button type="button" class="btn btn-secondary" data-dismiss="modal">Close</button>
