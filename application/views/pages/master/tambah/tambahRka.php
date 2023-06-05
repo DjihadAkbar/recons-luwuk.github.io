2ef31b7a8136802348be578179291d11d3c7ad27
Laptop Asus <mochdjihad@gmail.com>
2023-06-05 13:50:27 +0800

    Hari Senin

--- application/views/pages/master/tambah/tambahRka.php
+++ application/views/pages/master/tambah/tambahRka.php
@@ -13,7 +13,7 @@
                     <option value="">No Selected</option>
                     <?php foreach ($kapal as $row) : ?>
                         <option value="<?php echo $row['id']; ?>">
-                            <?php echo $row['kapal']; ?>
+                            <?php echo $row['kapal'].$row['id']; ?>
                         </option>
                     <?php endforeach; ?>
                 </select>
@@ -30,7 +30,7 @@
                     <option value="">No Selected</option>
                     <?php foreach ($lintasan as $row) : ?>
                         <option value="<?php echo $row['id']; ?>">
-                            <?php echo $row['lintasan']; ?>
+                            <?php echo $row['lintasan'].$row['id']; ?>
                         </option>
                     <?php endforeach; ?>
                 </select>
@@ -46,38 +46,12 @@
                     <option value="">No Selected</option>
                     <?php foreach ($pelabuhan as $row) : ?>
                         <option value="<?php echo $row['id_harbours']; ?>">
-                            <?php echo $row['pelabuhan']; ?>
+                            <?php echo $row['pelabuhan'].$row['id_harbours']; ?>
                         </option>
                     <?php endforeach; ?>
                 </select>
                 <?php
                 echo form_error('pelabuhan_asal');
                 ?>
-            </div>
-        </div>
-        <div class="form-group row">
-            <label for="tanggal_tiba" class="col-4 label-wrap"> Tanggal Tiba </label>
-
-            <div class="col-8">
-                <input class="form-control" type="date" id="tanggal_tiba" name="tanggal_tiba" value="2022-11-01" min="2022-11-01">
-            </div>
-        </div>
-        <div class="form-group row">
-            <label for="tanggal_berangkat" class="col-4 label-wrap"> Tanggal Berangkat </label>
-
-            <div class="col-8">
-                <input class="form-control" type="date" id="tanggal_berangkat" name="tanggal_berangkat" value="2022-11-01" min="2022-11-01">
-            </div>
-        </div>
-        <div class="form-group row">
-            <label for="waktu_tiba" class="col-4 label-wrap"> Waktu Tiba </label>
-            <div class="col">
-                <input class="form-control" type="time" id="waktu_tiba" name="waktu_tiba">
-            </div>
-        </div>
-        <div class="form-group row">
-            <label for="waktu_berangkat" class="col-4 label-wrap"> Waktu Berangkat </label>
-            <div class="col">
-                <input class="form-control" type="time" id="waktu_berangkat" name="waktu_berangkat">
             </div>
         </div>
