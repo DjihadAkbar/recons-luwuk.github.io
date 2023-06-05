e6ce50e5f62ef550a8b19f6823f4e7a725c8b050
Laptop Asus <mochdjihad@gmail.com>
2023-06-05 10:03:17 +0800

    Hari Senin

--- application/views/pages/dashboard.php
+++ application/views/pages/dashboard.php
@@ -2234,11 +2234,55 @@ color: black;" class="btn btn-link collapsed" data-toggle="collapse" data-target
                                                     </td>
                                                     <td>
                                                         <?php
-                                                            $originalTime = new DateTime($value["departure_date"]." ".$value['departure_time']);
-                                                            $targedTime = new DateTime($value["date"]." ".$value['time']);
-                                                            $interval = $originalTime->diff($targedTime);
-                                                            echo $interval->h.' Jam ';
-                                                            echo $interval->i.' Menit';
+                                                            // $originalTime = new DateTime($value["departure_date"]." ".$value['departure_time']);
+                                                            // $targedTime = new DateTime($value["date"]." ".$value['time']);
+                                                            $originalTime = strtotime($value["departure_date"]." ".$value['departure_time']);
+                                                            $targedTime = strtotime($value["date"]." ".$value['time']);
+                                                            // $interval = $originalTime->diff($targedTime);
+                                                            // echo $interval->h.' Jam ';
+                                                            // echo $interval->i.' Menit';
+                                                            $diff = abs($originalTime - $targedTime);
+ 
+                                                            // To get the year divide the resultant date into
+                                                            // total seconds in a year (365*60*60*24)
+                                                            $years = floor($diff / (365*60*60*24));
+                                                            
+                                                            // To get the month, subtract it with years and
+                                                            // divide the resultant date into
+                                                            // total seconds in a month (30*60*60*24)
+                                                            $months = floor(($diff - $years * 365*60*60*24)
+                                                                                            / (30*60*60*24));
+                                                            
+                                                            // To get the day, subtract it with years and
+                                                            // months and divide the resultant date into
+                                                            // total seconds in a days (60*60*24)
+                                                            $days = floor(($diff - $years * 365*60*60*24 -
+                                                                        $months*30*60*60*24)/ (60*60*24));
+                                                            
+                                                            // To get the hour, subtract it with years,
+                                                            // months & seconds and divide the resultant
+                                                            // date into total seconds in a hours (60*60)
+                                                            $hours = floor(($diff - $years * 365*60*60*24
+                                                                    - $months*30*60*60*24 - $days*60*60*24)
+                                                                                                / (60*60));
+                                                            
+                                                            // To get the minutes, subtract it with years,
+                                                            // months, seconds and hours and divide the
+                                                            // resultant date into total seconds i.e. 60
+                                                            $minutes = floor(($diff - $years * 365*60*60*24
+                                                                    - $months*30*60*60*24 - $days*60*60*24
+                                                                                        - $hours*60*60)/ 60);
+                                                            
+                                                            // To get the minutes, subtract it with years,
+                                                            // months, seconds, hours and minutes
+                                                            $seconds = floor(($diff - $years * 365*60*60*24
+                                                                    - $months*30*60*60*24 - $days*60*60*24
+                                                                            - $hours*60*60 - $minutes*60));
+                                                            
+                                                            // Print the result
+                                                            printf("%d years, %d months, %d days, %d hours, "
+                                                                . "%d minutes, %d seconds", $years, $months,
+                                                                        $days, $hours, $minutes, $seconds);
                                                         ?>
                                                     </td>
                                                     <td>
