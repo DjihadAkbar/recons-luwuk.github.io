52f364584985c7209d3bf27cf70d2c6a36eee0ab
laptopKantor <mochdjihad@gmail.com>
2023-07-14 10:26:59 +0800

    Hari ini

--- application/views/template/dashboard/body.php
+++ application/views/template/dashboard/body.php
@@ -197,14 +197,18 @@ if (!$this->session->userdata('logged_in'))
     }
 
     $(document).ready(function() {
-        var $title = "<?php echo $title; ?>"
-        if ($title != "Dashboard") {
-            $title = "<?php echo $title; ?>"
+        var title = "<?php echo "$title" ?>"
+        if (title != "Dashboard") {
+            title = "<?php echo "$title" ?>"
         } else {
-            $title = '';
+            title = '';
         }
 
         $('table.table-data').DataTable({
+            columnDefs: [{
+                    type: 'currency', targets: 0 
+                }
+            ],
             initComplete: function() {
                 this.api()
                     .columns()
@@ -228,27 +232,24 @@ if (!$this->session->userdata('logged_in'))
                     });
             },
             dom: 'Bfrtip',
-            columnDefs: [{
-                    type: 'currency', targets: 0 
-                }
-            ],
+            
             buttons: [{
                     extend: 'copy',
-                    filename: $title,
+                    filename: title,
                     className: 'btn btn-primary mr-1 mb-2',
                     text: '<i class="fas fa-copy"></i> ',
                     titleAttr: 'Copy to Clipboard',
                 },
                 {
                     extend: 'csv',
-                    filename: $title,
+                    filename: title,
                     className: 'btn btn-success mr-1 mb-2',
                     text: '<i class="fas fa-file-csv"></i> ',
                     titleAttr: 'Export to CSV',
                 },
                 {
                     extend: 'pdfHtml5',
-                    filename: $title,
+                    filename: title,
                     className: 'btn btn-danger mr-1 mb-2',
                     text: '<i class="fas fa-file-pdf"></i> ',
                     titleAttr: 'Export to PDF',
@@ -257,7 +258,7 @@ if (!$this->session->userdata('logged_in'))
                 },
                 {
                     extend: 'excelHtml5',
-                    filename: 'Ini Judulnya',
+                    filename: title,
                     className: 'btn btn-success mr-1 mb-2',
                     text: '<i class="fas fa-file-excel"></i> ',
                     titleAttr: 'Export to Excel',
@@ -269,7 +270,7 @@ if (!$this->session->userdata('logged_in'))
                 },
                 {
                     extend: 'print',
-                    filename: $title,
+                    filename: title,
                     className: 'btn btn-secondary mr-1 mb-2',
                     text: '<i class="fa fa-print"></i> ',
                     titleAttr: 'Imprimir',
@@ -277,9 +278,8 @@ if (!$this->session->userdata('logged_in'))
                     pageSize: 'LEGAL'
                 },
             ],
-
-
         });
+
         var table = $('table.table-data').DataTable();
         $('table.table-data tbody').on('click', 'tr', function() {
             if ($(this).hasClass('selected')) {
