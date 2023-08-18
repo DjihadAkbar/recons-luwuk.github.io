ddd483dc2be520ac4043cf11fec0b2d6dfdf0903
laptopKantor <mochdjihad@gmail.com>
2023-08-18 13:34:55 +0800

    Maintenance Mode

--- application/config/hooks.php
+++ application/config/hooks.php
@@ -15,6 +15,6 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 $hook['pre_system'][] = array(
     'class'    => 'maintenance_hook',
     'function' => 'offline_check',
-    'filename' => 'maintenance_hook.php',
+    'filename' => 'maintenance_hooks.php',
     'filepath' => 'hooks'
 );
